317fbf600d776df4e93e2f7135a7660cd55f7c55
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2022-05-20 19:39:36 -0300

    Create teste.sql

---
+++ teste.sql
@@ -0,0 +1,15 @@
+use dbUniversidade001;
+
+CREATE TABLE estudantes(
+id int ,
+nome varchar(50),
+idade int
+);
+
+CREATE TABLE professores(
+id int ,
+nome varchar(50),
+idade int
+);
+
+select * from professores;
